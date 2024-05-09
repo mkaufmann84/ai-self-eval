607a6ba9157389f70e39d52e8b2a5983db3b090a
max <max906993@gmail.com>
2024-05-08 23:59:02 -0400

    base version of g-eval usiong a form submission, multiple child. Has problem of re-rendering

--- src/app/_ai/Main.tsx
+++ src/app/_ai/Main.tsx
@@ -1,6 +1,14 @@
+"use client";
 import { Button } from "@/components/ui/button";
-import React from "react";
-import { test } from "./nlp";
+import React, {
+  Dispatch,
+  SetStateAction,
+  useCallback,
+  useEffect,
+  useRef,
+  useState,
+} from "react";
+import { createRubric, getOpenAI } from "./nlp";
 import Cookies from "js-cookie";
 import { COOKIES } from "@/constants";
 import { Label } from "@/components/ui/label";
@@ -21,44 +29,46 @@ import {
   FormMessage,
 } from "@/components/ui/form";
 import { z } from "zod";
-import { useForm } from "react-hook-form";
+import { set, useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { Input } from "@/components/ui/input";
 import { Textarea } from "@/components/ui/textarea";
-
+import { ChatCompletionMessageParam } from "openai/resources/index.mjs";
+import { v4 as uuidv4 } from "uuid";
+import { promptSystemAnalysis, messageScore, validateAndConvert } from "./nlp";
 const formSchema = z.object({
   prompt: z.string().min(1, "Prompt is required"),
   model: z.enum(["gpt-4-turbo", "gpt-3.5-turbo"]),
-  num_responses: z.coerce.number().positive(),
+  num_responses: z.coerce.number().positive().int(),
 });
 type FormSchema = z.infer<typeof formSchema>;
+
 export default function Main() {
   const [state, setState] = React.useState("");
   const handleResize = (event: any) => {
     const textarea = event.target;
-    textarea.style.height = "auto"; // Reset height to recalculate
-    textarea.style.height = `${textarea.scrollHeight}px`; // Set new height
+    textarea.style.height = "auto";
+    textarea.style.height = `${textarea.scrollHeight}px`;
   };
   const form = useForm<FormSchema>({
     resolver: zodResolver(formSchema),
     defaultValues: { prompt: "", num_responses: 1 },
   });
+  const [submitted, setSubmitted] = useState<null | {
+    formSchema: FormSchema;
+    id: string;
+  }>(null);
+  const handleSubmit = (data: FormSchema) => {
+    setSubmitted({ formSchema: data, id: uuidv4() });
+  };
+
   return (
     <div>
       <nav className="flex items-center gap-4 flex-wrap py-6">
         <Button>History</Button>
-        <Button
-          onClick={async () => {
-            const res = await test(Cookies.get(COOKIES.OPENAI_API_KEY) ?? "");
-            console.log(res);
-            setState(res ?? "failed");
-          }}
-        >
-          Click me
-        </Button>
       </nav>
       <Form {...form}>
-        <form onSubmit={form.handleSubmit((data) => console.log(data))}>
+        <form onSubmit={form.handleSubmit(handleSubmit)}>
           <div>
             <FormField
               control={form.control}
@@ -127,8 +137,255 @@ export default function Main() {
           <Button type="submit">Submit</Button>
         </form>
       </Form>
-      {state.length}
-      {state}
+      <div>
+        {submitted && (
+          /* I can change thetype of this */
+          <ResponseParent
+            input_form={submitted.formSchema}
+            id={submitted.id}
+          ></ResponseParent>
+        )}
+      </div>
     </div>
   );
 }
+interface ResponseSummary {
+  num_responses: number;
+  num_generated: number;
+  num_graded: number;
+  num_rubric: number;
+}
+interface ResponseParentProps {
+  input_form: FormSchema;
+  id: string;
+}
+type RubricRef = { promise: Promise<string>; settled: boolean } | undefined;
+
+const RP = ({ input_form, id }: ResponseParentProps) => {
+  console.log("rendering parent", id);
+  const responseRefs = useRef<ResponseData[]>([]);
+  const [summary, setSummary] = useState<ResponseSummary>({
+    num_responses: input_form?.num_responses ?? 0,
+    num_generated: 0,
+    num_graded: 0,
+    num_rubric: 0,
+  });
+  const rubricRef = useRef<RubricRef>();
+  useEffect(() => {
+    const run = async () => {
+      console.log("running");
+      rubricRef.current = {
+        promise: createRubric(input_form.prompt, input_form.model),
+        settled: false,
+      };
+      rubricRef.current.promise.then((rubric: string) => {
+        rubricRef.current!.settled = true;
+        setSummary((prev) => {
+          return { ...prev, num_rubric: 1 };
+        });
+      });
+    };
+    setSummary({
+      num_responses: input_form.num_responses,
+      num_generated: 0,
+      num_graded: 0,
+      num_rubric: 0,
+    });
+    run();
+    for (
+      let i = responseRefs.current.length;
+      i < input_form.num_responses;
+      i++
+    ) {
+      const response = {
+        id: uuidv4(),
+        response: "",
+        finished_response: false,
+        analysis: "",
+        finished_analysis: false,
+        score: null,
+      };
+      responseRefs.current.push(response);
+    }
+    responseRefs.current.sort((a, b) => {
+      if (a.score === null || b.score === null) {
+        return a.id < b.id ? -1 : 1;
+      }
+      return a.score - b.score;
+    });
+  }, []);
+
+  //in order to trigger a re-render, the child component will use the setSummary.
+  //I'd rather have the values be inferred though. Maybe have a useCallback()() that recalculates the summary.
+  const triggerUpdate = useCallback(() => {
+    const init: ResponseSummary = {
+      num_responses: 0,
+      num_generated: 0,
+      num_graded: 0,
+      num_rubric: rubricRef.current?.settled ? 1 : 0,
+    };
+    const new_summary = responseRefs.current.reduce((prev, curr) => {
+      prev.num_responses += 1;
+      if (curr.finished_response) {
+        prev.num_generated += 1;
+      }
+      if (curr.finished_analysis) {
+        prev.num_graded += 1;
+      }
+      return prev;
+    }, init);
+    console.log("setting summary");
+    setSummary(new_summary);
+  }, []);
+
+  if (!input_form) {
+    return null;
+  }
+
+  return (
+    <div>
+      {JSON.stringify(summary)}
+      {responseRefs.current.map((response) => {
+        console.log("mapping    " + response.id);
+        return (
+          <Response
+            key={response.id}
+            prompt={input_form.prompt}
+            model={input_form.model}
+            data={response}
+            rubricRef={rubricRef}
+            triggerUpdate={triggerUpdate}
+          ></Response>
+        );
+      })}
+    </div>
+  );
+};
+const ResponseParent = React.memo(RP);
+
+interface ResponseData {
+  id: string;
+  response: string;
+  finished_response: boolean;
+  analysis: string;
+  finished_analysis: boolean;
+  score: number | null;
+}
+
+const R = ({
+  prompt,
+  model,
+  data,
+  rubricRef,
+  triggerUpdate,
+}: {
+  prompt: string;
+  model: string;
+  data: ResponseData;
+  rubricRef: React.MutableRefObject<RubricRef>;
+  triggerUpdate: () => void;
+}) => {
+  const [text, setText] = React.useState("");
+  const [analysis, setAnalysis] = React.useState("");
+  const [score, setScore] = React.useState<number | null>(null);
+  useEffect(() => {
+    const render = async () => {
+      console.log("starting child");
+      const response_stream = await getOpenAI().chat.completions.create(
+        {
+          model: model,
+          messages: [{ role: "user", content: prompt }],
+          stream: true,
+        },
+        { idempotencyKey: data.id }
+      );
+      for await (const chunk of response_stream) {
+        if (chunk.choices[0].finish_reason === "stop") {
+          console.log("STOPIG");
+          break;
+        }
+        console.log(chunk.choices[0].delta.content);
+        data.response = data.response + chunk.choices[0].delta.content ?? "";
+        setText(data.response);
+      }
+      console.log("response", data.response);
+      data.finished_response = true;
+      triggerUpdate();
+
+      const rubric = (await rubricRef.current?.promise) ?? "";
+
+      const analysis_messages: ChatCompletionMessageParam[] = [
+        { role: "system", content: promptSystemAnalysis(rubric) },
+        { role: "user", content: data.response },
+      ];
+      const analysis_stream = await getOpenAI().chat.completions.create({
+        model: model,
+        messages: analysis_messages,
+        stream: true,
+      });
+      for await (const chunk of analysis_stream) {
+        data.analysis = data.analysis + chunk.choices[0]?.delta?.content ?? "";
+      }
+      data.finished_analysis = true;
+      setAnalysis(data.analysis);
+      triggerUpdate();
+
+      const score_text = await getOpenAI().chat.completions.create({
+        model: "gpt-4-turbo-preview",
+        messages: messageScore(analysis_messages, data.analysis),
+        response_format: { type: "json_object" },
+      });
+      let score;
+      try {
+        const score_json = JSON.parse(
+          score_text.choices[0].message.content ?? "{}"
+        );
+        score = validateAndConvert(score_json.score);
+        data.score = score;
+      } catch {
+        data.score = 0;
+      } finally {
+        setScore(data.score);
+        triggerUpdate();
+      }
+    };
+    render();
+  }, []);
+
+  return (
+    <div className="border-red-300 border-2 p-4">
+      <h1>Response</h1>
+      <p>{text}</p>
+      <div className="w-full h-2 my-20 bg-slate-500 "></div>
+      <h1>Analysis</h1>
+      <p>{analysis}</p>
+      <div className="w-full h-2 my-20 bg-slate-500 "></div>
+      <h1>Score</h1>
+      <p>{score}</p>
+    </div>
+  );
+};
+const Response = React.memo(R);
+
+/** The way I'm going to do this is after they submit the form, it renders x components and generates a rubroc.
+ *  Each component has a state, and the parent will contain an array of these states
+ * each component generates its responses. Then awaits rubric, and then will start the analysis.
+ * So responses, analysis happens in each component. Not sure how I want to do sorting.
+ *
+ * For sort, I could have a sidebar that contains the score. And clicking that scrolls to the correct content
+ * I could also have a button that sorts
+ * Or after everything has finished, I sort automatically.
+ *
+ * There can be a toggleSort or alertParent, so the parent knows a child has finished.
+ * Then it can determine whether tho change the three states, and whether it is done and can be sorted.
+ *
+ * I could have an editing state that disables button while they are editing the rubric.
+ * For now, I'm not going to show the rubric or analysis
+ *
+ * I can have a state of the sort order. State could also be inferred, and just recalculating when there is an update.
+ * ***Better yet, I don't have to have a state of the sort order. It only sorts once everything has finished.
+ */
+
+/* use memo so that when sort order changes, it doesn't re-render compnennt */
+/* I don't need to handle the changing of a sort order because it will only happen once all components have finished.  */
+/* State doesn't have to be known in child. Child is just the display of a state hosted in parent.  */
--- src/app/_ai/nlp.ts
+++ src/app/_ai/nlp.ts
@@ -1,15 +1,107 @@
+"use client";
 import OpenAI from "openai";
+import Cookies from "js-cookie";
+import { COOKIES } from "@/constants";
+import { ChatCompletionMessageParam } from "openai/resources/index.mjs";
+import { z } from "zod";
 
-export async function test(key: string) {
+export const getOpenAI = () => {
   const openai = new OpenAI({
-    apiKey: key,
+    apiKey: Cookies.get(COOKIES.OPENAI_API_KEY),
     dangerouslyAllowBrowser: true,
   });
-  const chatCompletion = await openai.chat.completions.create({
-    messages: [{ role: "user", content: "What is 1 + 1? Give an analysis" }],
-    model: "gpt-3.5-turbo",
+  return openai;
+};
+
+export function promptSystemAnalysis(rubric: string) {
+  const formatted = `Your task is to write an analysis on an input text using a rubric.
+    You will receive a rubric to guide you. You must use the rubric.
+    The rubric contains a criteria and steps you can use to evaluate the criteria.
+    Please make sure you read and understand these instructions carefully. Please keep this document open while reviewing, and refer to it as needed.
+    
+
+    Rubric:
+    ${rubric}
+    `;
+  return formatted;
+}
+export function messageScore(
+  prev_messages: ChatCompletionMessageParam[],
+  analysis: string
+) {
+  const messages: ChatCompletionMessageParam[] = [
+    ...prev_messages,
+    { role: "assistant", content: analysis },
+    {
+      role: "system",
+      content: `Using the analysis you just did, return a JSON response that scores the input text on a scale of 0-100.
+      To format, you should use the key "score" to represent the score.`,
+    },
+  ];
+  return messages;
+}
+
+export function evalCriteriaMessages(input_prompt: string) {
+  const formatted = `Imagine you are a professor. You need a way to grade your students' responses to an input prompt.
+  Your task is to create an evaluation criteria for a response based on an input prompt.
+  This is like a rubric because it will help grade a response to the input prompt.
+  The critera will be used to grade a response on a scale of 0-100.
+
+  Input prompt: 
+  ${input_prompt}
+
+  Evaluation Criteria (Rubric) for a response:
+  `;
+  const messages: ChatCompletionMessageParam[] = [
+    { role: "user", content: formatted },
+  ];
+  return messages;
+}
+
+export function evalStepsMessages(eval_criteria: string) {
+  const formatted = `Imagine you are a professor. You need a way to grade your students' responses to an input prompt.
+  You have an evaluation criteria. However, you need the steps in order to evaluate a response.
+  Your task is to generate instructions on how to use the evaluation criteria. 
+  
+  Input prompt: 
+  ${prompt}
+
+  Evaluation Criteria for a response:
+  ${eval_criteria}
+  
+  Evaluation Steps:`;
+  const messages: ChatCompletionMessageParam[] = [
+    { role: "user", content: formatted },
+  ];
+  return messages;
+}
+
+export async function createRubric(input_prompt: string, model: string) {
+  const eval_criteria = await getOpenAI().chat.completions.create({
+    model: model,
+    messages: evalCriteriaMessages(input_prompt),
   });
-  console.log(chatCompletion.choices[0].message.content);
-  console.log(chatCompletion);
-  return chatCompletion.choices[0].message.content;
+  const eval_steps = await getOpenAI().chat.completions.create({
+    model: model,
+    messages: evalStepsMessages(eval_criteria.choices[0].message.content ?? ""),
+  });
+  const formatted = `Evaluation Criteria:
+  ${eval_criteria.choices[0].message.content ?? ""}
+  
+  Evaluation Steps:
+  ${eval_steps.choices[0].message.content ?? ""}
+  `;
+  return formatted;
+}
+
+export function validateAndConvert(input: string): number {
+  const numberSchema = z.preprocess((input) => {
+    const num = parseFloat(input as any);
+    return isNaN(num) ? NaN : Math.round(num);
+  }, z.number().int().min(0).max(100));
+  try {
+    return numberSchema.parse(input);
+  } catch (error) {
+    return 0;
+  }
 }
